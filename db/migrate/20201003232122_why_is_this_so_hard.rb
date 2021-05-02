e9026579aa09fba760cd14a9ed6783bd99f8d938
Steve Pletcher <spletche@broadinstitute.org>
2021-05-02 13:10:45 -0400

    make migrations suck way less

--- db/migrate/20201003232122_why_is_this_so_hard.rb
+++ db/migrate/20201003232122_why_is_this_so_hard.rb
@@ -1,20 +1,4 @@
 class WhyIsThisSoHard < ActiveRecord::Migration[5.2]
   def change
-    drop_table :resolutions
-
-    create_table :resolutions do |t|
-      t.references :conflict, foreign_key: true
-      t.references :resolution, foreign_key: true
-      t.integer :player_id
-      t.integer :beneficiary_player_id
-      t.string :burned_trait_type
-      t.string :type
-      t.string :player_roll_result
-      t.string :gm_roll_result
-      t.string :state
-      t.boolean :succeeded
-
-      t.timestamps
-    end
   end
 end

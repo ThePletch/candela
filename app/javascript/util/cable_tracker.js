3b90a1c5729ad9e958b9269bed5f1bc4540e411f
Steve Pletcher <spletche@broadinstitute.org>
2021-05-06 08:23:20 -0400

    Sort participations by position in state reducer

--- app/javascript/util/cable_tracker.js
+++ app/javascript/util/cable_tracker.js
@@ -10,8 +10,23 @@ export function createCableStateManager(channelName, channelParams, receivedCall
     });
 };
 
-export function listStateReducer(currentList, receivedData) {
+// sortExtractor is a function that extracts the sorting key from a record.
+export function listStateReducer(currentList, receivedData, sortExtractor) {
     const existingIndex = currentList.findIndex(record => record.id === receivedData.id);
+
+    // sort by ID by default
+    sortExtractor = sortExtractor ?? (record => Number(record.id));
+
+    const sortFn = (a, b) => {
+        const sortResult = sortExtractor(a) - sortExtractor(b);
+
+        if (sortResult == NaN) {
+            console.error("NaN-causing records:", a, b)
+            throw "Got NaN from sorting records, stopping JS from proceeding anyway"
+        }
+
+        return sortResult;
+    };
 
     if (existingIndex >= 0) {
         currentList[existingIndex] = receivedData;
@@ -19,5 +34,5 @@ export function listStateReducer(currentList, receivedData) {
         currentList.push(receivedData)
     }
 
-    return currentList;
+    return currentList.sort(sortFn);
 };
